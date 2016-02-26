fd6a455d47071af9c4f471c7d202b94f0c306d4f
Naohiro IWAMOTO <mayok@club.kyutech.ac.jp>
2016-02-26 13:43:55 +0900

    add test

--- spec/lita/handlers/echo_spec.rb
+++ spec/lita/handlers/echo_spec.rb
@@ -1,4 +1,11 @@
 require "spec_helper"
 
 describe Lita::Handlers::Echo, lita_handler: true do
+  it { is_expected.to route("echo") }
+  it { is_expected.to route("echo").to(:echo) }
+
+  it "echoes back text" do
+    send_message("hoge")
+    expect(replies.last).to include("hoge")
+  end
 end
